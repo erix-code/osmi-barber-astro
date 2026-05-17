29eabcafef995d89a54bd26d8d0a1def4fd7b6e2
erick.t <ermazte@gmail.com>
2026-05-17 23:42:51 +0000

    Change config for nginx.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
-
+import node from '@astrojs/node'; // Cambia esto
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify(),
+  adapter: node({
+    mode: 'standalone', // Esto permite que Astro corra como un servidor propio
+  }),
   vite: {
     plugins: [tailwindcss()]
   }
